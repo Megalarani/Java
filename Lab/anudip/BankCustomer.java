cc7594c58cfeca493c429cd4b65457a219fb7b84
Megalarani <103100018+Megalarani@users.noreply.github.com>
2022-09-29 16:22:55 +0530

    Add files via upload

---
+++ Lab/anudip/BankCustomer.java
@@ -0,0 +1,15 @@
+package anudip;
+public class BankCustomer
+{
+public void details()
+{
+   String accno="1234567890123";  
+   String cust_name="Ravi";  
+   String atype="Savings";  
+   long amount=50000; 
+   System.out.println("Account Number: "+accno); 
+   System.out.println("Customer Name: "+cust_name); 
+   System.out.println("Account Type: "+atype); 
+   System.out.println("Amount: "+amount);  
+}
+}
